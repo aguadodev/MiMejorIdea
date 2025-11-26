b4b3329339334178f267712bd6971cb8cbfdb939
Óscar <aguado@edu.xunta.es>
2025-11-26 13:53:34 +0100

    Creando Perfil Usuario

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
+
+final class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('index/about.html.twig', [
+            'controller_name' => 'AboutController',
+        ]);
+    }
+
+#[Route('/mail', name: 'app_mail')]
+public function sendEmail(MailerInterface $mailer): Response
+{
+    $email = (new Email())
+        ->from('hello@example.com')
+        ->to('aguadoaudiovisual@gmail.com')
+        //->cc('cc@example.com')
+        //->bcc('bcc@example.com')
+        //->replyTo('fabien@example.com')
+        //->priority(Email::PRIORITY_HIGH)
+        ->subject('Enviando correo de prueba desde la aplicación!')
+        ->text('Contenido en texto plano!')
+        ->html('<p>Contenido en formato HTML. Se pueden aplicar plantillas Twig para mayor flexibilidad y reutilización</p>');
+
+
+    $mailer->send($email);
+   
+    dd($email);
+}
+    
+}
